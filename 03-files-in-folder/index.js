988f1823302c8c997e1545c736dc08da5668e905
Zmicer-Yafremau <dmitry.mickevich@gmail.com>
2022-05-21 23:15:13 +0300

    init:solve 03-file-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const arr=[];
+function st(file){
+
+    fs.stat(path.join(__dirname,'secret-folder',file),(error,stats)=>{
+        if (error) {console.log(error);};
+        console.log(path.basename(file, path.extname(file))+' - '+path.extname(file).slice(1)+' - '+(stats.size/1024).toFixed(3) +' kB');
+
+    });
+};
+fs.readdir( path.join(__dirname,'secret-folder'),{ withFileTypes: true }, (err,files)=>{
+    if (err) console.log(err);
+    files.forEach(file=>{
+        if (file.isFile()){
+st(file.name);
+        }
+       
+    });
+})
+
