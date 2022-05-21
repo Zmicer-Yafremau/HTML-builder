b9b4d110666864f7cb157543f96bba2d95dfead4
Zmicer-Yafremau <dmitry.mickevich@gmail.com>
2022-05-21 23:43:20 +0300

    fix:remove unnesesury data

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const arr=[];
 function st(file){
 
     fs.stat(path.join(__dirname,'secret-folder',file),(error,stats)=>{
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,8 @@
-
+const fs = require('fs');
+const path = require('path');
+fs.mkdir(path.join(__dirname,'files-copy'),err=>{
+if (err) throw err;
+});
+fs.copyFile(path.join(__dirname,'files','test-text.txt'),path.join(__dirname,'files-copy','test-text.txt'),err=>{
+    if (err) throw err;
+    });
