23479fd997d7c7f15d47abd69820ef753b91c93b
Zmicer-Yafremau <dmitry.mickevich@gmail.com>
2022-05-21 23:52:45 +0300

    init:solve 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,6 +3,18 @@ const path = require('path');
 fs.mkdir(path.join(__dirname,'files-copy'),err=>{
 if (err) throw err;
 });
-fs.copyFile(path.join(__dirname,'files','test-text.txt'),path.join(__dirname,'files-copy','test-text.txt'),err=>{
-    if (err) throw err;
+
+
+fs.readdir( path.join(__dirname,'files'),{ withFileTypes: true }, (err,files)=>{
+    if (err) console.log(err);
+    files.forEach(file=>{
+        if (file.isFile()){
+            fs.copyFile(path.join(__dirname,'files',file.name),path.join(__dirname,'files-copy',file.name),err=>{
+                if (err) throw err;
+                });
+        }
+       
     });
+})
+
+
