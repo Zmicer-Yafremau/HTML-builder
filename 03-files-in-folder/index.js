b71a2e8058c39d5a8218cf02292cf764df3d66ee
antipachita <nice.strogonov@inbox.ru>
2022-05-24 13:19:49 +0300

    fix: update functional for 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -10,17 +10,17 @@ fs.readdir(way, {withFileTypes: true}, function(err, items) {
         console.log(`${items[i].name.split('.').slice(0, -1).join('.')} - ${path.extname(items[i].name)} - ${stats.size} bytes`);
                     
       });
-                 
-    } else if (items[i].isDirectory() === true) {
-      fs.readdir(`03-files-in-folder/secret-folder/${items[i].name}`, function(err, files) {
-        for (let n=0; n<files.length; n++) {
-          fs.stat(`03-files-in-folder/secret-folder/${items[i].name}/${files[n]}`, (error, stats) => {
-            console.log(`${files[n]} - ${path.extname(files[n])} - ${stats.size} bytes`);
+    }           
+    //  else if (items[i].isDirectory() === true) {
+    //   fs.readdir(`03-files-in-folder/secret-folder/${items[i].name}`, function(err, files) {
+    //     for (let n=0; n<files.length; n++) {
+    //       fs.stat(`03-files-in-folder/secret-folder/${items[i].name}/${files[n]}`, (error, stats) => {
+    //         console.log(`${files[n]} - ${path.extname(files[n])} - ${stats.size} bytes`);
                             
-          });
-        }
-      });
-    }
+    //       });
+    //     }
+    //   });
+    // }
             
             
   }
